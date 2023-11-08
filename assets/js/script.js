ceffabaeb28a1762cef46b4923de75f48ace3147
Liene Briede <lienesjaunkundze@gmail.com>
2023-11-08 08:16:24 +0000

    Add javascript structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame()
+
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
+function checkAnswer()
+
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
+function calculateCorrectAnswer()
+
+/**
+* Gets the current score from the DOM and increments it by 1
+*/
+function incrementScore()
+
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementWrongAnswer()
+
+function displayAdditionQuestion()
+
+function displaySubtractQuestion()
+
+function displayMultiplyQuestion
 

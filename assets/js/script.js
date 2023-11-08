f4ab93f03339a8ccd33e42fc0814682b4e36a7ce
Liene Briede <lienesjaunkundze@gmail.com>
2023-11-08 12:22:41 +0000

    Add calculate function

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,19 @@ function checkAnswer()
  * Gets the operands (the numbers) and the operator (plus, minus etc)
  * directly from the dom, and returns the correct answer.
  */
-function calculateCorrectAnswer()
+function calculateCorrectAnswer() {
 
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "adition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
+}
 /**
 * Gets the current score from the DOM and increments it by 1
 */
